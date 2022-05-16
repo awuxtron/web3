fb550a30ce0a4de3359aa7e5bda9ebf8c7e9373b
Awuxtron <awuxtron@gmail.com>
2022-05-16 19:52:11 +0700

    feat: update

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,7 +7,8 @@ $finder = Finder::create()
     ->in(__DIR__ . '/config')
     ->in(__DIR__ . '/src')
     ->ignoreDotFiles(true)
-    ->ignoreVCS(true);
+    ->ignoreVCS(true)
+;
 
 return (new Config)->setFinder($finder)->setRules([
     '@PHP81Migration' => true,
@@ -17,4 +18,12 @@ return (new Config)->setFinder($finder)->setRules([
     'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
     'no_superfluous_phpdoc_tags' => false,
     'single_line_comment_style' => false,
+    'global_namespace_import' => ['import_classes' => true, 'import_constants' => true, 'import_functions' => true],
+    'control_structure_continuation_position' => true,
+    'phpdoc_line_span' => true,
+    'date_time_create_from_format_call' => true,
+    'declare_parentheses' => true,
+    'nullable_type_declaration_for_default_null_value' => true,
+    'simplified_if_return' => true,
+    'simplified_null_return' => true,
 ]);
